56096a91b031100873b4948444f93fb35029d130
Manojna6 <72754918+Manojna6@users.noreply.github.com>
2021-04-25 13:26:23 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,17 @@
-p1 =[];
-p2 =[];
+var inputs = [];
+var inputs1 = [];
 
 function get_Paragraph1(){
-    p1 = [
-        document.getElementById("para1_input1").value + 
-        document.getElementById("para1_input2").value +
-        document.getElementById("para1_input3").value +
-        document.getElementById("para1_input4").value +
-        document.getElementById("para1_input5").value +
-        document.getElementById("para1_input6").value];
-    document.getElementById("para1").innerHTML = p1;
+    for(var i=1; i<=6 ; i++)
+    {
+        inputs.push(document.getElementById("para1_input"+i).value);
+        document.getElementById("showParagraph1").innerHTML = inputs.join(". ");
+    }
 };
 function get_Paragraph2(){
-    p2 = [
-        document.getElementById("para2_input1").value + 
-        document.getElementById("para2_input2").value +
-        document.getElementById("para2_input3").value +
-        document.getElementById("para2_input4").value +
-        document.getElementById("para2_input5").value +
-        document.getElementById("para2_input6").value];
-    document.getElementById("para2").innerHTML = p2;
+    for(var i=1; i<=6 ; i++)
+    {
+        inputs1.push(document.getElementById("para2_input"+i).value);
+        document.getElementById("showParagraph2").innerHTML = inputs1.join(". ");
+    }
 };
-
-
-
